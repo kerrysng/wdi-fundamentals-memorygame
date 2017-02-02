f43b239a4b33f4f4a89315c3488b64de98b95ca6
Kerry Sng <Kerry Sng>
2017-02-02 18:30:33 +1000

    created game logi

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,4 +2,10 @@ console.log("JS file is connected to HTML! Woo!")
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
-var cardFour = "king"
+var cardFour = "king";
+
+/*if(cardTwo === cardFour) {
+	alert('You found a match!'');
+};
+else { alert('Sorry, try again');
+}; */
