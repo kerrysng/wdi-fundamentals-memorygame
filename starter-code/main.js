d02f37b453e85c740237e81ec50bd78068adf70a
Kerry Sng <Kerry Sng>
2017-02-05 17:56:20 +1000

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -17,7 +17,35 @@ function createCards = function() {
 		
 		var cardElement = document.createElement('div');
 		cardElement.className = 'card';
+        cardElement.setAttribute('data-card', cards[i]);
+        cardElement.addEventListener('click', isTwoCards);
+
+		board.appendChild(cardElement);
 		board.appendChild(cardElement);
 
 	}
 
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src= "C:\Users\kerrysng\Desktop\General Assembly\kingCard.png">";
+	} else {
+		this.innerHTML = "<img src= "C:\Users\kerrysng\Desktop\General Assembly\queenCard.png">";
+	}
+	
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+}
+
+function isMatch(cards) {
+	if (cards[0] === cards[1]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.")
+	}
+}
