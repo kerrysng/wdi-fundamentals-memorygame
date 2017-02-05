44227c039013e84229f850324a2b61d639d04aa3
Kerry Sng <Kerry Sng>
2017-02-05 17:28:40 +1000

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,8 +4,20 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-/*if(cardTwo === cardFour) {
-	alert('You found a match!'');
+ if(cardTwo === cardFour) {
+	alert('You found a match!');
 };
 else { alert('Sorry, try again');
-}; */
+}; 
+
+var board = document.getElementById('game-board');
+
+function createCards = function() {
+	for (var i = 0; i<4; i++) {
+		
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		board.appendChild(cardElement);
+
+	}
+
